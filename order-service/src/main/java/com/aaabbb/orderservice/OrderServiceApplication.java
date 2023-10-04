5ef842deca5c820610d06b4065fc637ffd04a834
TangciuYueng <2310360939@qq.com>
2023-10-04 11:55:45 +0800

    test prom & gra

--- order-service/src/main/java/com/aaabbb/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/aaabbb/orderservice/OrderServiceApplication.java
@@ -3,11 +3,15 @@ package com.aaabbb.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class OrderServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 		SpringApplication.run(OrderServiceApplication.class, args);
+		InetAddress localhost = InetAddress.getLocalHost();
+		System.out.println(localhost.getHostAddress());
 	}
-
 }
